8375fa2696093693d8dc20fcac1ed5f31d684fb7
fjbatresv <fjbatresv@gmail.com>
2020-04-26 22:12:54 -0600

    version of environments hidden values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,15 @@
 export const environment = {
   production: true,
-  url_api: 'https://platzi-store.herokuapp.com'
+  url_api: 'https://platzi-store.herokuapp.com',
+  url_sentry: '********************',
+  firebase: {
+    apiKey: '************************',
+    authDomain: '************************',
+    databaseURL: '************************',
+    projectId: '************************',
+    storageBucket: '************************',
+    messagingSenderId: '************************',
+    appId: '************************',
+    measurementId: '************************',
+  },
 };
